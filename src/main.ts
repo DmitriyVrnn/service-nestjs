498b7820689f30f51ea976850b9ed65aab60ce72
Dmitriy Voronin <dmitriy.vrnn@gmail.com>
2021-05-11 01:24:22 +0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nestjs API')
+    .setDescription('Документация для API')
+    .setVersion('1.0.0')
+    .build();
+
+  const documentSwagger = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, documentSwagger);
   await app.listen(3000);
 }
+
 bootstrap();
